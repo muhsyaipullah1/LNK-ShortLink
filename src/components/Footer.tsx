ae356c5e305420b458e8d1add793e5df1ad65c9f
muhsyaipullah1 <muhsyaipullah14@gmail.com>
2026-01-06 16:00:16 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
                         </svg>
                     </div>
-                    <span className="footer-text">© 2024 LNK - ShortLink. By SyaLink.com</span>
+                    <span className="footer-text">© 2026 LNK - ShortLink. By SyaLink.com</span>
                 </div>
                 <div className="footer-links">
                     <a href="/privacy">Privacy</a>
